fb3541f401eada3b97fb21cde0e93e1d957c7f98
naveennk045 <naveennk045@gmail.com>
2025-08-06 06:12:19 +0530

    refactor: add movieservice for handling the movies

--- MovieBookingSystem/src/App.java
+++ MovieBookingSystem/src/App.java
@@ -1,9 +1,4 @@
-// Movie Booking System: App Entry Point
-//    javac -cp ./src -d . src/models/*.java src/services/*.java App.java
-import models.Movie;
-import models.User;
-import java.util.ArrayList;
-import java.util.List;
+
 import services.userService;
 
 public class App {
@@ -14,17 +9,5 @@ public class App {
         user.addUser();
 
 
-        List<Movie> movies = new ArrayList<>();
-        movies.add(new Movie("Interstellar", 4.8f, "2014-11-07"));
-        movies.add(new Movie("Inception", 4.7f, "2010-07-16"));
-        movies.add(new Movie("The Dark Knight", 4.9f, "2008-07-18"));
-        movies.add(new Movie("Dune", 4.4f, "2021-10-22"));
-        movies.add(new Movie("Avatar", 4.5f, "2009-12-18"));
-
-
-        System.out.println("\n--------------------- Available Movies ---------------------");
-        for (Movie movie : movies) {
-            movie.showDetails();
-        }
     }
 }
